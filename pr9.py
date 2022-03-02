6a516ceed944e0cb415f40da5c1ffc10e8368170
Vasiliy Mameev <test.vasiliy1@gmail.com>
2022-03-02 15:15:50 +0300

    Ninth stepic homework

--- pr9.py
+++ pr9.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import math
 import time
-link = " http://SunInJuly.github.io/execute_script.html"
+link = " http://SunInJuly.github.io/execute_script.html"#Переход по ссылке
 try:
     browser = webdriver.Chrome()
     browser.get(link)
@@ -11,11 +11,11 @@ try:
     x = x_element.text
     y = calc(x)
     browser.execute_script("window.scrollTo(0, 150)")
-    input1 = browser.find_element( By.ID, 'answer')
+    input1 = browser.find_element(By.ID, 'answer')
     input1.send_keys(y)
-    option1 = browser.find_element_by_id("robotCheckbox")
+    option1 = browser.find_element(By.ID, "robotCheckbox")
     option1.click()
-    option2 = browser.find_element_by_id("robotsRule")
+    option2 = browser.find_element(By.ID,"robotsRule")
     option2.click()
     current_dir = os.path.abspath(os.path.dirname(__file__))
     file_name = "vasiliytest.txt"
